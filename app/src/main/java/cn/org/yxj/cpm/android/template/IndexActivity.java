c013f417346fea492dd0d2ddc944be53b03c4895
ellise <ellise@qq.com>
2020-01-23 14:49:22 +0800

    实验成功，onViewCreated只能通过remove和add获得

--- app/src/main/java/cn/org/yxj/cpm/android/template/IndexActivity.java
+++ app/src/main/java/cn/org/yxj/cpm/android/template/IndexActivity.java
@@ -15,9 +15,9 @@ public class IndexActivity extends AbstractWeexActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
-        setContainer((ViewGroup) findViewById(R.id.index_container));
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        getWindow().setFormat(PixelFormat.TRANSLUCENT);
+        setContainer((ViewGroup) findViewById(R.id.help));
+        //getWindow().setFormat(PixelFormat.TRANSLUCENT);
+
+        renderPageByURL("http://dotwe.org/raw/dist/2bbe1860da4669a68595c1aed01d7fd2.bundle.wx");
     }
 }
--- app/src/main/java/cn/org/yxj/cpm/android/template/MainActivity.java
+++ app/src/main/java/cn/org/yxj/cpm/android/template/MainActivity.java
@@ -1,37 +1,46 @@
 package cn.org.yxj.cpm.android.template;
 
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import androidx.appcompat.app.AppCompatActivity;
 import org.apache.weex.IWXRenderListener;
 import org.apache.weex.WXSDKInstance;
 import org.apache.weex.common.WXRenderStrategy;
+import org.apache.weex.commons.WXAnalyzerDelegate;
 import org.apache.weex.utils.WXFileUtils;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity implements IWXRenderListener {
     WXSDKInstance mWXSDKInstance;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+
+        Rect outRect = new Rect();
+        this.getWindow().getDecorView().getWindowVisibleDisplayFrame(outRect);
         mWXSDKInstance = new WXSDKInstance(this);
         mWXSDKInstance.registerRenderListener(this);
-        mWXSDKInstance.onActivityCreate();
-        /**
-         * bundleUrl source http://dotwe.org/vue/38e202c16bdfefbdb88a8754f975454c
-         */
-        String pageName = "WXSample";
-        String bundleUrl = "http://dotwe.org/raw/dist/38e202c16bdfefbdb88a8754f975454c.bundle.wx";
-        //String bundleUrl = "file://assets/bundle.wx";
-        mWXSDKInstance.renderByUrl(pageName, bundleUrl, null, null, WXRenderStrategy.APPEND_ASYNC);
 
-//        mWXSDKInstance.render(pageName, WXFileUtils.loadAsset("bundle.wx", this),
-//                null, null,
-//                WXRenderStrategy.APPEND_ASYNC);
+        mWXSDKInstance.onActivityCreate();
+        this.getWindow().setFormat(-3);
+
+        String url = "http://dotwe.org/raw/dist/38e202c16bdfefbdb88a8754f975454c.bundle.wx";
+        Map<String, Object> options = new HashMap();
+        options.put("bundleUrl", url);
+        mWXSDKInstance.setTrackComponent(true);
+        mWXSDKInstance.renderByUrl("WXSample", url, options, null, WXRenderStrategy.APPEND_ASYNC);
     }
     @Override
     public void onViewCreated(WXSDKInstance instance, View view) {
-        setContentView(view);
+        //setContentView(view);
+        // todo 只能通过add / remove 获得
+        ViewGroup contentView= findViewById(android.R.id.content);
+        contentView.addView(view);
     }
     @Override
     public void onRenderSuccess(WXSDKInstance instance, int width, int height) {
